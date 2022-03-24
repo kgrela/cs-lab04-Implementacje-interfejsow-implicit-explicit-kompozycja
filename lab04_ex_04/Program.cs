4120380d1081601a549d6789f4608d156c6f8dea
Kacper Grela <kacper.grela@microsoft.wsei.edu.pl>
2022-03-24 16:32:51 +0100

    Task 4

--- lab04_ex_04/Program.cs
+++ lab04_ex_04/Program.cs
@@ -6,7 +6,37 @@ namespace lab04_ex_04
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Copier x = new Copier();
+            Console.WriteLine($"Current copier state: {x.GetState()}");
+
+            ((IDevice)x).PowerOn();
+            Console.WriteLine($"Current copier state: {x.GetState()}");
+
+
+            var doc = new PDFDocument("doc.pdf");
+            ((IPrinter)x).Print(doc);
+            ((IPrinter)x).Print(doc);
+            ((IPrinter)x).Print(doc);
+
+            // cool down
+            ((IPrinter)x).Print(doc);
+            ((IPrinter)x).Print(doc);
+
+            IDocument doc1;
+            IDocument doc2;
+            IDocument doc3;
+
+            ((IScanner)x).Scan(out doc1);
+            ((IScanner)x).Scan(out doc2);
+
+            // cool down
+            ((IScanner)x).Scan(out doc3);
+            x.GetCounter();
+
+            Console.WriteLine($"Current copier state: {x.GetState()}");
+
+            ((IDevice)x).PowerOff();
+            Console.WriteLine($"Current copier state: {x.GetState()}");
         }
     }
 }
