c969cbee839ac9afb9ebd3036e759614a92566fe
Kacper Grela <kacper.grela@microsoft.wsei.edu.pl>
2022-03-24 16:30:40 +0100

    Task 4

---
+++ lab04_ex_04/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab04_ex_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
